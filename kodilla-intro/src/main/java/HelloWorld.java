43175750d971749e3ab5686efcb83f51c9a55b19
Katarzyna Kołodziej <katarzyna.kolodziej1@gmail.com>
2024-05-03 00:53:21 +0200

    Hello World 0,7

--- kodilla-intro/src/main/java/HelloWorld.java
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld {
+    public static void main(String[]args){
+    System.out.println("Hello World!");
+    System.out.println("Moje imię to Kaśka");
+    System.out.println("Mój wiek można już nazwać jako doświadczony");
+    System.out.println("Pierwsza litera mojego nazwiska to K");
+}
 }

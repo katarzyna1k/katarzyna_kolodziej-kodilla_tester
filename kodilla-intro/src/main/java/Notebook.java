cdb95c1824ad8af12b015f825ae3fe56c96e577d
Katarzyna Kołodziej <katarzyna.kolodziej1@gmail.com>
2024-05-18 23:27:35 +0200

    Zadanie moduł 2 Notebook

---
+++ kodilla-intro/src/main/java/Notebook.java
@@ -0,0 +1,73 @@
+public class Notebook {
+    int weight;
+    int price;
+    int year;
+
+    public Notebook(int weight, int price, int year) {
+        this.weight = weight;
+        this.price = price;
+        this.year = year;
+    }
+
+    public static void main(String[] args) {
+
+
+        Notebook notebook = new Notebook(600, 1500, 2020);
+        System.out.println("Notebook: " + notebook.weight + "  " + notebook.price + " " + notebook.year);
+        notebook.checkPrice();
+        notebook.checkWeight();
+        notebook.checkYear();
+
+        Notebook heavyNotebook = new Notebook(2000, 2000, 2022);
+        System.out.println("Heavy Notebook: " + heavyNotebook.weight + " " + heavyNotebook.price + " " + heavyNotebook.year);
+        heavyNotebook.checkPrice();
+        heavyNotebook.checkWeight();
+        heavyNotebook.checkYear();
+
+        Notebook oldNotebook = new Notebook(1600, 900, 2010);
+        System.out.println("Old Notebook: " + oldNotebook.weight + " " + oldNotebook.price + " " + oldNotebook.year);
+        oldNotebook.checkPrice();
+        oldNotebook.checkWeight();
+        oldNotebook.checkYear();
+
+        Notebook newNotebook = new Notebook(1200, 800, 2020);
+        System.out.println("New Notebook: " + newNotebook.weight + " " + newNotebook.price + " " + newNotebook.year);
+        newNotebook.checkPrice();
+        newNotebook.checkWeight();
+        newNotebook.checkYear();
+    }
+
+    public void checkPrice() {
+
+        if (this.price < 600) {
+            System.out.println("This notebook is cheap.");
+        } else if (this.price >= 600 && this.price <= 1000) {
+            System.out.println("This price is good.");
+        } else {
+            System.out.println("This notebook is expensive.");
+        }
+
+    }
+
+    public void checkWeight() {
+
+        if (this.weight >= 2000) {
+            System.out.println("This is a heavy weight notebook.");
+        } else if (this.weight < 2000 && this.weight > 700) {
+            System.out.println("This notebook has medium weight.");
+        } else {
+            System.out.println("It is not a heavy notebook.");
+        }
+    }
+
+    public void checkYear() {
+
+        if (this.year > 2015 && this.price > 600) {
+            System.out.print("New is always pricey.");
+        } else if (this.year > 2015 && this.price < 600) {
+            System.out.println("It's not true, new is cheeper!");
+        } else {
+            System.out.println("You can not tell if year of production determinate price of a notebook.");
+        }
+    }
+}
--- kodilla-intro/src/main/java/WhileRandom.java
+++ kodilla-intro/src/main/java/WhileRandom.java
@@ -1,16 +1,30 @@
 import java.util.Random;
+
 public class WhileRandom {
+
+
     public static void main(String[] args) {
-        public int getCountOfRandomNumber ( int max){
-            Random random = new Random();
-            int result = 0;
-            int sum = 0;
-            while (sum < max) {
-                int temp = random.nextInt(10);
-                sum = sum + temp;
-                result++;
-            }
-            return result;
+        //System.out.println(getCountOfRandomNumber(15));
+        WhileRandom nowyObiekt= new WhileRandom();
+
+        int nowaZmienna= nowyObiekt.getCountOfRandomNumber (15);
+        System.out.println(nowaZmienna);
+
+
+
+
+    }
+
+     int getCountOfRandomNumber(int max) {
+        Random random = new Random();
+        int result = 0;
+        int sum = 0;
+        while (sum < max) {
+            int temp = random.nextInt(10);
+            sum = sum + temp;
+            result++;
+
         }
+        return result;
     }
 }

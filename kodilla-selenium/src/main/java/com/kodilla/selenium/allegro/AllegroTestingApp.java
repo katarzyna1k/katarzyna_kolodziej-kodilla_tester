f6a5694e754aa9335330384172a478ea186ad2db
Katarzyna Kołodziej <katarzyna.kolodziej1@gmail.com>
2024-09-17 18:36:35 +0200

    Do 13.4

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -11,18 +11,16 @@ public class AllegroTestingApp {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\katar\\selenium-drivers\\Chrome\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
         ChromeOptions chromeOptions = new ChromeOptions();
-//        chromeOptions.addArguments("--headless");
-        chromeOptions.addArguments("--remote-debugging-port=9222");
-        //chromeOptions.addArguments("--remote-allow-origins=*");
+        chromeOptions.addArguments("--remote-allow-origins=*");
         WebDriver driver = new ChromeDriver(chromeOptions);
-        driver.get("https://allegro.pl");
+        driver.get("https://ebay.pl");
 
 
-        WebElement mavicMiniElement = driver.findElement(By.xpath("//input[contains(@placeholder, 'czego')]"));
+        WebElement mavicMiniElement = driver.findElement(By.xpath("//input[contains(@placeholder, 'Wyszukaj')]"));
         mavicMiniElement.sendKeys("Mavic mini");
 
-        WebElement allegroElement = driver.findElement(By.xpath("//*[@label='Kategorie']/option[3]"));
+        WebElement allegroElement = driver.findElement(By.xpath("//*[@id='gh-cat']"));
         Select elektronika = new Select(allegroElement);
-        elektronika.selectByVisibleText("Elektronika");
+        elektronika.selectByVisibleText("Fotografia i Kamery");
     }
 }
